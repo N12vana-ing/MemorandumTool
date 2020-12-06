cc1f0c2877d2c1a2b44512e720710c6b76fbc152
zhoukai <www.708875859@qq.com>
2020-12-06 22:15:43 +0800

    add vue

--- memorandum_web/src/main/java/com/zyk/web/WebStartApplication.java
+++ memorandum_web/src/main/java/com/zyk/web/WebStartApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableSwagger2Doc
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class WebStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebStartApplication.class,args);
